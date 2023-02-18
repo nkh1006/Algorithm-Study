4de4dd803ad5a766130eebbc515c51aaf36af7d8
NANGGI <mqsora@naver.com>
2023-02-18 22:13:28 +0900

    Add files via upload

---
+++ programmers/134240.js
@@ -0,0 +1,11 @@
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/134240
+ */
+
+function solution(food) {
+  return food.reverse().reduce((acc, cur, idx) => {
+      const calorie = (food.length - idx - 1).toString();
+      const repeatedFood = calorie.repeat(parseInt(cur / 2));
+      return repeatedFood + acc + repeatedFood;
+  }, '0');
+}
