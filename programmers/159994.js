1bf706a02d305b6fe794763e9aca16d98a99c578
NANGGI <mqsora@naver.com>
2023-02-22 15:52:43 +0900

    Add files via upload

---
+++ programmers/159994.js
@@ -0,0 +1,16 @@
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/159994
+ */
+
+function solution(cards1, cards2, goal) {
+  let j = 0;
+  let k = 0;
+  
+  for(let i = 0; i < goal.length; i++){
+      if(goal[i] == cards1[j]) j++;
+      else if(goal[i] == cards2[k]) k++;
+      else return "No";
+  }
+  
+  return "Yes";
+}
